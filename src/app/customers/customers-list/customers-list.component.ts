7d30e54c27036112260fcd25c4884fdc46362122
Younes Zaid <zaidyounes94@gmail.com>
2018-10-22 22:14:56 +0100

    interfaces implementation & Interpolation syntaxe

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 
+import { ICustomer } from "src/app/shared/interfaces";
+
 @Component({
   selector: "app-customers-list",
   templateUrl: "./customers-list.component.html"
 })
 export class CustomersListComponent implements OnInit {
-  filteredCustomers: any[] = [];
+  filteredCustomers: ICustomer[] = [];
   customersOrderTotal: number;
   currencyCode: string = "USD";
 
