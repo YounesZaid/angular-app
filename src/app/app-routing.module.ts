db25875eb593f5f8e86cf78fc9c761a35c594c8d
Younes Zaid <zaidyounes94@gmail.com>
2018-11-08 10:38:01 +0100

    added the appRouting

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes, Router } from "@angular/router";
+
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "./employes" },
+  { path: "**", pathMatch: "full", redirectTo: "./employes" }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { EmployesModule } from "./employes/employes.module";
 import { AppComponent } from "./app.component";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core/core.module";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  imports: [BrowserModule, CoreModule, EmployesModule, SharedModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    EmployesModule,
+    SharedModule,
+    AppRoutingModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
